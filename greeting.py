22c1f3157e31ecfc2ab43c1a015413f07feeaea7
Rjoyadet <57276368+Rjoyadet@users.noreply.github.com>
2025-01-28 13:14:35 -0600

    Update greeting.py
    added an if else function

--- greeting.py
+++ greeting.py
@@ -1,2 +1,7 @@
 Print("greetings lovely bags of mostly water")
 feeling = input("How are you today?")
+  if feeling == 'good' or 'fine':
+    print("I am glad to hear that.")
+  else:
+    print("I am sorry your day isn't well.")
+print("what shows are you currently watching?")
